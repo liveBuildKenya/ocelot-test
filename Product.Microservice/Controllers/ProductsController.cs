be56055cd610dcc51c6edca572c5eb5864df15aa
Ezra Mungai <ezra.mungai@sibasi.ltd>
2020-12-03 21:24:33 +0300

    Initial Commit

---
+++ Product.Microservice/Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Product.Microservice.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductsController : ControllerBase
+    {
+
+        public ProductsController()
+        {
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Product Microservice";
+        }
+    }
+}
---
+++ User.Microservice/Controllers/UsersController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace User.Microservice.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsersController : ControllerBase
+    {
+
+        public UsersController()
+        {
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "User micrservice";
+        }
+    }
+}
